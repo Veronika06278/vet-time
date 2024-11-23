037556042354c97860d57328d87d069dc83f0a61
Veronika06278 <veronika.zheleva2006@gmail.com>
2024-11-23 12:02:33 +0200

    Started Vet model

---
+++ VetTime/VetTime.Data.Models/Veterinarian.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VetTime.Data.Models
+{
+    public class Veterinarian
+    {
+        [Required]
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; } = null!;
+
+        //TODO:Navigation Properties
+    }
+}
