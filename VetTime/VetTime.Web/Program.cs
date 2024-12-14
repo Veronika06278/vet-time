273103c71e0156eb899cd8bf35d6ea6d71f5d422
Veronika06278 <veronika.zheleva2006@gmail.com>
2024-12-14 12:12:18 +0200

    Set up connection string in config file

--- VetTime/VetTime.Web/Program.cs
+++ VetTime/VetTime.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using VetTime.Data;
+using VetTime.Data.Models;
 
 namespace VetTime.Web
 {
@@ -16,7 +17,8 @@ namespace VetTime.Web
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole<Guid>>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
