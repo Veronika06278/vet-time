2e205a29f7a5e3d48efdb86e3ed5a341b54743bd
Veronika06278 <veronika.zheleva2006@gmail.com>
2025-04-27 11:09:08 +0300

    Seperated Home controller and vet controller

--- VetTime/VetTime.Web/Controllers/HomeController.cs
+++ VetTime/VetTime.Web/Controllers/HomeController.cs
@@ -57,78 +57,7 @@ namespace VetTime.Web.Controllers
             return View(model);
         }
 
-        [HttpPost]
-        public IActionResult Recommendations(HomeViewModel model)
-        {
-            //if (!ModelState.IsValid)
-            //{
-             
-            //    model.Cities = _cityService.GetAllCityNames();
-            //    model.Specializations = _specializationService.GetAllSpecializations();
-            //    return View(model);
-            //}
-
-            model.Cities = _cityService.GetAllCityNames();
-            model.Specializations = _specializationService.GetAllSpecializations();
-            model.Vets = _vetService
-                .GetVetsInformation(/*model.Id, */model.Specialization, model.CityName, model.VetFirstName, model.VetLastName);
-
-            return View(model);
-        }
-
-        [HttpGet]
-        public IActionResult Details(Guid id)
-        {
-            var model = _vetService.GetVetDetails(id);
-            if (model == null)
-                return NotFound();
-
-            return View(model);
-        }
-
-        [HttpPost]
-        public IActionResult BookAppointment(Guid appointmentId)
-        {
-            var appointment = _dbContext.Appointments
-                .FirstOrDefault(a => a.Id == appointmentId);
-            if (appointment == null)
-                return NotFound();
-
-            appointment.HasVisited = true;
-            _dbContext.SaveChanges();
-            return RedirectToAction(nameof(Details), new { id = appointment.VetId });
-        }
         
-
-        [HttpGet]
-        public IActionResult SelectSlot(Guid id)
-        {
-            // id is the veterinarian's Id
-            var vetDetails = _vetService.GetVetDetails(id);
-            if (vetDetails == null) return NotFound();
-
-            var model = new CreateAppointmentViewModel
-            {
-                VetId = id,
-                AvailableSlots = vetDetails.AvailableSlots
-            };
-            return View(model);
-        }
-
-        [HttpPost]
-        public IActionResult ConfirmSlot(CreateAppointmentViewModel model)
-        {
-            // model.SelectedSlotId holds the appointment slot to book
-            // Mark booked in database
-            var slot = _dbContext.Appointments.Find(model.SelectedSlotId);
-            if (slot == null) return NotFound();
-
-            slot.HasVisited = true;
-            _dbContext.SaveChanges();
-
-            // Redirect back to details of the vet
-            return RedirectToAction("Details", new { id = model.VetId });
-        }
 
 
 
